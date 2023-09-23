b71f3e543737be7158fa6a3582d483d8878b932c
Bobby Naluz <bnaluz9@gmail.com>
2023-09-23 14:51:57 -0400

    initial commit

---
+++ problems/11-flatten.js
@@ -0,0 +1,35 @@
+/***********************************************************************
+Write a recursive function called `flatten` that takes a single array with
+any number of nested arrays and returns and array with all the nested
+contents on one level.
+
+Do not use the built in Array.flat.
+
+Examples:
+
+flatten([]); // []
+flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
+***********************************************************************/
+
+function flatten(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+
+    //recursive case //recursive step
+    if (Array.isArray(el)) {
+      newArray.push(...flatten(el));
+    } else {
+      newArray.push(el);
+    }
+  }
+  return newArray;
+}
+
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([])); // []
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+module.exports = flatten;
---
+++ problems/12-is-sorted.js
@@ -0,0 +1,42 @@
+/***********************************************************************
+Write a recursive solution called `isSorted` to determine if the input array
+is sorted in ascending order.
+
+Examples:
+
+isSorted([1, 2, 3, 4, 5]); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
+***********************************************************************/
+
+function isSorted(arr) {
+  if (arr.length === 1) {
+    return true;
+  }
+  if (arr[0] > arr[1]) {
+    // console.log('0 less than', arr[0]);
+    // console.log('1 less than ', arr[1]);
+    return false;
+  }
+  if (arr[0] < arr[1]) {
+    // console.log('0', arr[0]);
+    // console.log('1', arr[1]);
+    arr.shift();
+    // console.log('1st');
+    // console.log(arr);
+    return isSorted(arr);
+  }
+}
+
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = isSorted;
+} catch (e) {
+  module.exports = null;
+}
